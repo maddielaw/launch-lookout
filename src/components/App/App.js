80ecc6cec46ba5c4409555fda0b8224708c7f710
Madison Law <madisonlaw1@gmail.com>
2022-04-19 20:13:43 -0600

    Add JSX setup

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,27 @@
 import React from 'react';
+import NextLaunch from '../NextLaunch/NextLaunch';
+import UpcomingLaunches from '../UpcomingLaunches/UpcomingLaunches';
 import './App.css';
 
 
-
 const App = () => {
+
   return (  
     <main className='App'>
-      <h1>Launch Lookout</h1>
+
+      <section className='main-section'>
+        <nav className='nav'>
+          <h1 className='header'>🔭 Launch Lookout</h1>
+          <button>My Bookmarked Launches</button>
+        </nav>
+        <section>
+          <NextLaunch />
+          <UpcomingLaunches />
+        </section>
+      </section>
+      <section className='space-news'>
+      </section>
+
     </main>
   );
 }
