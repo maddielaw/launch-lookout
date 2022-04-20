c584c04bcc480fbb078ce005b17e4c62433a2af6
Madison Law <madisonlaw1@gmail.com>
2022-04-19 21:33:21 -0600

    Build method to map out and render launches from API

--- src/components/UpcomingLaunches/UpcomingLaunches.js
+++ src/components/UpcomingLaunches/UpcomingLaunches.js
@@ -1,14 +1,33 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { DataContext } from '../../contexts/DataContext';
+import Launch from '../Launch/Launch';
 import './UpcomingLaunches.css'
 
 const UpcomingLaunches = () => {
+  const launchInfo = useContext(DataContext);
+
+  const sortLaunches = () => {
+    const updatedLaunchInfo = launchInfo.upcomingLaunches.filter((data, i) => i !== 0)
+    return updatedLaunchInfo.map(launch => {
+      return <Launch 
+      mission={launch.mission}
+      name={launch.name} 
+      launchDate={launch.window_start} 
+      company={launch.launch_service_provider.name} 
+      image={launch.image} 
+      key={launch.id}
+      id={launch.id}
+      />
+    })
+  }
+
   return (  
     <section className='upcoming-launches-container'>
       <div className='upcoming-header'>
         <h2>Upcoming Launches</h2>
       </div>
       <div className='upcoming-cards-container'>
-        <p>launch cards go here</p>
+        {sortLaunches()}
       </div>
     </section>
   );
