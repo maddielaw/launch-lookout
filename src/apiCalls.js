384bbb08c957f1f911fbc3d5eeca0ad250fef302
Madison Law <madisonlaw1@gmail.com>
2022-04-19 21:41:15 -0600

    Increase fetch to 20 launches

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,5 +1,5 @@
 export const fetchUpcomingLaunches = () => {
-  return fetch('https://spacelaunchnow.me/api/ll/2.2.0/launch/upcoming/?limit=10&mode=detailed')
+  return fetch('https://spacelaunchnow.me/api/ll/2.2.0/launch/upcoming/?limit=20&mode=detailed')
   .then((response) => {
     if (!response.ok) {
       throw new Error("Error fetching data. Please try again later.");
