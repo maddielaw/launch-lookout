5f1a5fc76688c7ad01e39e0d98fd736098c9b913
Madison Law <madisonlaw1@gmail.com>
2022-04-19 20:14:30 -0600

    Build out render for NextLaunch

--- src/components/NextLaunch/NextLaunch.js
+++ src/components/NextLaunch/NextLaunch.js
@@ -2,8 +2,16 @@ import React from 'react';
 import './NextLaunch.css'
 
 const NextLaunch = () => {
+  
   return (  
-    <h1>I'm the next upcoming launch section</h1>
+    <section className='next-launch-section'>
+      <h2>Next Launch: Falcon 9 | Starlink</h2>
+      <p>April 21, 2022 | Launch Window Start: 15:16:00</p>
+      <div className='next-launch-btn-container'>
+        <button>Bookmark this launch</button>
+        <button>View launch details</button>
+      </div>
+    </section>
   );
 }
  
