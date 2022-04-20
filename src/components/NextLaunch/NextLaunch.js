b9039b66ed27b217d80c1dea33eebaa604b8c4fe
Madison Law <madisonlaw1@gmail.com>
2022-04-19 21:41:36 -0600

    Hook up next launch to API

--- src/components/NextLaunch/NextLaunch.js
+++ src/components/NextLaunch/NextLaunch.js
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { DataContext } from '../../contexts/DataContext';
 import './NextLaunch.css'
 
 const NextLaunch = () => {
-  
+  const launchInfo = useContext(DataContext)
+  const nextLaunch = launchInfo.upcomingLaunches[0]
+
   return (  
     <section className='next-launch-section'>
-      <h2>Next Launch: Falcon 9 | Starlink</h2>
-      <p>April 21, 2022 | Launch Window Start: 15:16:00</p>
+      <h2>Next Launch: {nextLaunch.name}</h2>
+      <p>{nextLaunch.window_start}</p>
       <div className='next-launch-btn-container'>
         <button>Bookmark this launch</button>
         <button>View launch details</button>
