1065bd21375b37153dc562cb0af0e6bc78bd1c4e
Madison Law <madisonlaw1@gmail.com>
2022-04-20 10:33:08 -0600

    Made network request dynamic

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,5 +1,5 @@
-export const fetchUpcomingLaunches = () => {
-  return fetch('https://spacelaunchnow.me/api/ll/2.2.0/launch/upcoming/?limit=20&mode=detailed')
+export const fetchUpcomingData = (query) => {
+  return fetch(`https://spacelaunchnow.me/api/ll/2.2.0/${query}/upcoming/?limit=20&mode=detailed`)
   .then((response) => {
     if (!response.ok) {
       throw new Error("Error fetching data. Please try again later.");
