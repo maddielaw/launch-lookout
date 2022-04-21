245d398b4557052e9067887f651fb8a347d0fec4
Madison Law <madisonlaw1@gmail.com>
2022-04-21 12:21:44 -0600

    Build out boilerplate render for savedLaunch card

--- src/components/SavedLaunch/SavedLaunch.js
+++ src/components/SavedLaunch/SavedLaunch.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './SavedLaunch.css'
 
-const SavedLaunch = () => {
-  return (  
-    <h1>I'm a saved launch card on bookmarks page</h1>
+const SavedLaunch = ({id, name, company, image, mission, launchDate, removeBookmark}) => {
+
+  return (
+    <section className='saved-launch-card'>
+      <div className='date-delete'>
+        <p>{launchDate}</p>
+        <button>X</button>
+      </div>
+      <div className='saved-launch-details'>
+        <img className='saved-launch-img'src={image} />
+        <h2>{name}</h2>
+        <p>{company}</p>
+        <p>{mission}</p>
+        <Link to={`/launches/${id}`}><button>View Launch Details</button></Link>
+      </div>
+    </section>
+
+
   );
 }
  
