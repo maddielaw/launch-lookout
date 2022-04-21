34364d427effd5f6634f752e0217c83adce2ee31
Madison Law <madisonlaw1@gmail.com>
2022-04-20 18:37:59 -0600

    Change URL routing to id

--- src/components/Launch/Launch.js
+++ src/components/Launch/Launch.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import './Launch.css'
 
 
-const Launch = ({name, launchDate, company, image, mission}) => {
+const Launch = ({ id, name, launchDate, company, image, mission}) => {
   return (
     <section className='launch-card'>
       <div className='img-company-container'>
@@ -15,7 +15,7 @@ const Launch = ({name, launchDate, company, image, mission}) => {
         <h3>{name}</h3>
         {mission ? <p className='launch-mission-description'>{mission.description}</p> : <p>No mission description available at this time!</p>}
         <div className='launch-card-btn-container'>
-          <Link to={`/launches/${name}`}>
+          <Link to={`/launches/${id}`}>
             <button>View Launch Details</button>
           </Link>
           <button>Bookmark this Launch</button>
