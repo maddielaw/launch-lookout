2ffaec382f6b989e3ecadf719e6cc82b95eceef0
Madison Law <madisonlaw1@gmail.com>
2022-04-21 10:32:42 -0600

    Build out Bookmark Context

--- src/contexts/BookmarkContext.js
+++ src/contexts/BookmarkContext.js
@@ -1,10 +1,19 @@
-import React, { createContext } from 'react';
+import React, { createContext, useState } from 'react';
 
-const BookmarkContext = createContext;
+const BookmarkContext = createContext();
 
-const BookmarkContextProvider = () => {
+const BookmarkContextProvider = ({ children }) => {
+
+  const [bookmarks, setBookmarks] = useState([]);
+
+  const removeBookmark = (launch) => {
+    let index = bookmarks.map(bookmark => {return bookmark.name}).indexOf(launch)
+    bookmarks.splice(index, 1)
+  }
+
+
   return (  
-    <BookmarkContext.Provider>
+    <BookmarkContext.Provider value={{ bookmarks, setBookmarks, removeBookmark }}>
       {children}
     </BookmarkContext.Provider>
   );
