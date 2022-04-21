7f32f60f2b8a740f11749e3fa787297737eb8e60
Madison Law <madisonlaw1@gmail.com>
2022-04-21 12:05:51 -0600

    Build out Bookmarks component

--- src/components/Bookmarks/Bookmarks.js
+++ src/components/Bookmarks/Bookmarks.js
@@ -1,10 +1,37 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { Link } from 'react-router-dom';
+import { BookmarkContext } from '../../contexts/BookmarkContext';
+import SavedLaunch from '../SavedLaunch/SavedLaunch';
 import './Bookmarks.css'
 
 
 const Bookmarks = () => {
-  return (  
-    <h1>I'm the bookmarks section</h1>
+  const bookmarkData = useContext(BookmarkContext);
+
+  const renderBookmarkCards = () => {
+    return bookmarkData.bookmarks.map(bookmark => {
+      return <SavedLaunch 
+        id={bookmark.id} 
+        key={bookmark.id} 
+        name={bookmark.name} 
+        company={bookmark.company} 
+        image={bookmark.image} 
+        mission={bookmark.mission} 
+        removeBookmark={bookmarkData.removeBookmark}
+      />
+    })
+  }
+
+  return (
+    <section className='bookmark-section'>
+      <div className='bookmark-header'>
+        <h1>🚀 My Bookmarked Launches</h1>
+        <Link to='/'><button>X</button></Link>
+      </div>
+      <div className='bookmark-container'>
+        {renderBookmarkCards()}
+      </div>
+    </section>
   );
 }
  
