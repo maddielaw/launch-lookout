aaa99d6c42e99977d6265dd499c3140ca50e4c70
Madison Law <madisonlaw1@gmail.com>
2022-04-22 12:14:06 -0600

    Build out Loader component and style

---
+++ src/components/Loader/Loader.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import blackhole from '../../assets/blackhole.png'
+import './Loader.css'
+
+
+const Loader = () => {
+  return ( 
+    <section className='loading-page'>
+      <h1>Launch Lookout is preparing for liftoff</h1>
+      <img className='blackhole-spinning' src={blackhole} alt={'blackhole spinning loading'}></img>
+    </section>
+  );
+}
+ 
+export default Loader;
