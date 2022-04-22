99bd4f5c327e941892994f75a8a54bda291bb993
Madison Law <madisonlaw1@gmail.com>
2022-04-22 12:45:14 -0600

    Update loader message

--- src/components/Loader/Loader.js
+++ src/components/Loader/Loader.js
@@ -1,13 +1,16 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import blackhole from '../../assets/blackhole.png'
+import { DataContext } from '../../contexts/DataContext';
 import './Loader.css'
 
 
 const Loader = () => {
+  const loadingInfo = useContext(DataContext);
+
   return ( 
     <section className='loading-page'>
-      <h1>Launch Lookout is preparing for liftoff</h1>
-      <img className='blackhole-spinning' src={blackhole} alt={'blackhole spinning loading'}></img>
+        <h1>Launch Lookout preparing for liftoff!</h1>
+        <img className='blackhole-spinning' src={blackhole} alt={'blackhole spinning loading'}></img>
     </section>
   );
 }
