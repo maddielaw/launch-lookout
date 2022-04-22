897e1fa142823b70fb168dedbcfa585fbe7a6870
Madison Law <madisonlaw1@gmail.com>
2022-04-22 11:55:19 -0600

    Add class names for styling

--- src/components/Error/Error.js
+++ src/components/Error/Error.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './Error.css'
 
 
 const Error = () => {
   return (  
     <section className='error-page'>
-      <nav className='nav'>
+      <nav className='error-nav'>
         <h1 className='header'>🔭 Launch Lookout</h1>
-        <Link to='/'><button className='back-to-main'>Back to Main</button></Link>
-        </nav>
-      <h1>Failure to liftoff!</h1>
+      </nav>
+      <div className='error-text'>
+        <h1>Failure to liftoff!</h1>
+        <Link to='/'><button className='error-back-to-main'>Back to Main</button></Link>
+      </div>
     </section>
   );
 }
