bb2e8a72845d5325e6697713795616c49be647bc
Madison Law <madisonlaw1@gmail.com>
2022-04-22 11:42:06 -0600

    Add catch and finally blocks for error handling

--- src/contexts/DataContext.js
+++ src/contexts/DataContext.js
@@ -1,4 +1,5 @@
 import React, { createContext, useState, useEffect } from 'react';
+import Error from '../components/Error/Error';
 import { fetchUpcomingData } from '../apiCalls';
 
 const DataContext = createContext();
@@ -6,19 +7,27 @@ const DataContext = createContext();
 const DataContextProvider = ({ children }) => {
 
   const [upcomingLaunches, setUpcomingLaunches] = useState(null);
+  const [error, setError] = useState(false)
+  const [isLoading, setIsLoading] = useState(false)
 
   useEffect(() => {
+    setIsLoading(true)
     fetchUpcomingData('launch')
       .then((data) => {
-        setUpcomingLaunches({
-          upcomingLaunches: data.results
-        })
+        setUpcomingLaunches(data.results)
       })
+      .catch(err => setError(true))
+      .finally(() => setIsLoading(false))
   }, [])
 
   return (  
-    <DataContext.Provider value={upcomingLaunches}>
+    <DataContext.Provider value={{upcomingLaunches, error, setError, isLoading}}>
       {upcomingLaunches && children}
+      {error ? (
+        <React.Fragment>
+          <Error />
+        </React.Fragment>
+      ) : null}
     </DataContext.Provider>
   );
 }
