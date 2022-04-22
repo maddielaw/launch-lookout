3dd367716bc346bc8fbc095aba10ce05f359f4e6
Madison Law <madisonlaw1@gmail.com>
2022-04-22 11:41:01 -0600

    Add routing for Error component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,6 +4,7 @@ import { BookmarkContextProvider } from '../../contexts/BookmarkContext';
 import { DataContextProvider } from '../../contexts/DataContext';
 import Bookmarks from '../Bookmarks/Bookmarks';
 import NextLaunch from '../NextLaunch/NextLaunch';
+import Error from '../Error/Error';
 import SingleLaunchPage from '../SingleLaunchPage/SingleLaunchPage';
 import SpaceNewsContainer from '../SpaceNewsContainer/SpaceNewsContainer';
 import UpcomingLaunches from '../UpcomingLaunches/UpcomingLaunches';
@@ -14,18 +15,18 @@ const App = () => {
 
   return (  
     <main className='App'>
-      <Switch>
-
-        <DataContextProvider>
-          <BookmarkContextProvider>
+      <DataContextProvider>
+        <BookmarkContextProvider>
+          <Switch>
+            
             <Route exact path='/'>
               <section className='main-section'>
                 <nav className='nav'>
                   <h1 className='header'>🔭 Launch Lookout</h1>
                   <Link to='/bookmarks'><button className='bookmarks-page-btn'>My Bookmarked Launches</button></Link>
                 </nav>
-                      <NextLaunch />
-                      <UpcomingLaunches />
+                  <NextLaunch />
+                  <UpcomingLaunches />
               </section>
               <section className='space-news'>
                 <SpaceNewsContainer />
@@ -41,10 +42,15 @@ const App = () => {
               render={({ match }) => {
                 return <SingleLaunchPage id={match.params.id}/>
               }}/>
-          </BookmarkContextProvider>
-        </DataContextProvider>
 
-    </Switch>
+            <Route>
+              <Error />
+            </Route>
+
+          </Switch>
+        </BookmarkContextProvider>
+      </DataContextProvider>
+
     </main>
   );
 }
