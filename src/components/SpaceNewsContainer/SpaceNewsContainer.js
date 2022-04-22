888a02ea727d54ef22e395fa1915611c4673c78d
Madison Law <madisonlaw1@gmail.com>
2022-04-22 12:44:55 -0600

    Add ADORABLE astronaut loader for events to accomodate lag

--- src/components/SpaceNewsContainer/SpaceNewsContainer.js
+++ src/components/SpaceNewsContainer/SpaceNewsContainer.js
@@ -4,13 +4,15 @@ import { fetchUpcomingData } from '../../apiCalls';
 import Event from '../Event/Event';
 import { format } from 'date-fns';
 import { DataContext } from '../../contexts/DataContext';
+import Loader from '../Loader/Loader';
+import astronaut from '../../assets/astronaut.png'
 
 
 const SpaceNewsContainer = () => {
   
   const [upcomingEvents, setUpcomingEvents] = useState(null)
   const errorInfo = useContext(DataContext);
-
+  const [isEventLoading, setIsEventLoading] = useState(false)
 
   const sortEvents = () => {
     return upcomingEvents.map(item => {
@@ -27,19 +29,26 @@ const SpaceNewsContainer = () => {
   }
 
   useEffect(() => {
+    setIsEventLoading(true)
     fetchUpcomingData('event')
       .then((data) => {
         setUpcomingEvents(data.results)
       })
       .catch(err => errorInfo.setError(true))
+      .finally(setIsEventLoading(false))
   }, [])
 
   return (
     <section className='events-section'>
       <h2 className='events-header'>Upcoming Events in Spaceflight</h2>
-      <div className='events-container'>
-        {upcomingEvents && sortEvents()}
-      </div>
+        <div className='events-container'>
+          {upcomingEvents && !isEventLoading ? sortEvents() :
+          <div className='astronaut-loader'>
+            <h3>Events loading...</h3>
+            <img className='astronaut-spinning' src={astronaut} alt={'astronaut spinning loading'}></img>
+          </div>
+          }
+        </div>
     </section>
   );
 }
