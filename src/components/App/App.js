5fc616ed4639ba6f1c25868e4e050d3f244128fa
Madison Law <madisonlaw1@gmail.com>
2022-04-24 14:20:35 -0600

    Format App

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,59 +1,58 @@
-import React from 'react';
-import { Route, Switch, Link } from 'react-router-dom';
-import { BookmarkContextProvider } from '../../contexts/BookmarkContext';
-import { DataContextProvider } from '../../contexts/DataContext';
-import Bookmarks from '../Bookmarks/Bookmarks';
-import NextLaunch from '../NextLaunch/NextLaunch';
-import Error from '../Error/Error';
-import SingleLaunchPage from '../SingleLaunchPage/SingleLaunchPage';
-import SpaceNewsContainer from '../SpaceNewsContainer/SpaceNewsContainer';
-import UpcomingLaunches from '../UpcomingLaunches/UpcomingLaunches';
-import './App.css';
-import {IoIosRocket} from 'react-icons/io'
-
+import React from "react";
+import { Route, Switch, Link } from "react-router-dom";
+import { BookmarkContextProvider } from "../../contexts/BookmarkContext";
+import { DataContextProvider } from "../../contexts/DataContext";
+import Bookmarks from "../Bookmarks/Bookmarks";
+import NextLaunch from "../NextLaunch/NextLaunch";
+import Error from "../Error/Error";
+import SingleLaunchPage from "../SingleLaunchPage/SingleLaunchPage";
+import SpaceNewsContainer from "../SpaceNewsContainer/SpaceNewsContainer";
+import UpcomingLaunches from "../UpcomingLaunches/UpcomingLaunches";
+import "./App.css";
+import { IoIosRocket } from "react-icons/io";
 
 const App = () => {
-
-  return (  
-    <main className='App'>
+  return (
+    <main className="App">
       <DataContextProvider>
         <BookmarkContextProvider>
           <Switch>
-            
-            <Route exact path='/'>
-              <section className='main-section'>
-                <nav className='nav'>
-                  <h1 className='header'><IoIosRocket/>Launch Lookout</h1>
-                  <Link to='/bookmarks'><button className='bookmarks-page-btn'>My Bookmarked Launches</button></Link>
+            <Route exact path="/">
+              <section className="main-section">
+                <nav className="nav">
+                  <h1 className="header">
+                    <IoIosRocket />
+                    Launch Lookout
+                  </h1>
+                  <Link to="/bookmarks">
+                    <button className="bookmarks-page-btn">My Bookmarked Launches</button>
+                  </Link>
                 </nav>
-                  <NextLaunch />
-                  <UpcomingLaunches />
+                <NextLaunch />
+                <UpcomingLaunches />
               </section>
-              <section className='space-news'>
+              <section className="space-news">
                 <SpaceNewsContainer />
               </section>
             </Route>
-
-            <Route exact path='/bookmarks'>
+            <Route exact path="/bookmarks">
               <Bookmarks />
             </Route>
-
-            <Route 
-              exact path='/launches/:id'
+            <Route
+              exact
+              path="/launches/:id"
               render={({ match }) => {
-                return <SingleLaunchPage id={match.params.id}/>
-              }}/>
-
+                return <SingleLaunchPage id={match.params.id} />;
+              }}
+            />
             <Route>
               <Error />
             </Route>
-
           </Switch>
         </BookmarkContextProvider>
       </DataContextProvider>
-
     </main>
   );
-}
- 
+};
+
 export default App;
