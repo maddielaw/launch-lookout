6139f3e327311277e96f2fdfd0eb4b854ad4ef2d
Madison Law <madisonlaw1@gmail.com>
2022-04-23 18:28:35 -0600

    Update tests to remove emojis

--- cypress/integration/single_launch_spec.js
+++ cypress/integration/single_launch_spec.js
@@ -12,7 +12,7 @@ const singleLaunch = () => {
         mission_patches: [{image_url: "https://spacelaunchnow-prod-east.nyc3.cdn.digitaloceanspaces.com/media/mission_patch_images/space2520x252_mission_patch_20210606182401.png"}],
         mission: {description: "A batch of 53 satellites for Starlink mega-constellation - SpaceX's project for space-based Internet communication system.", type: "Communications", orbit: {name: 'Low Earth Orbit'}},
         image: "https://spacelaunchnow-prod-east.nyc3.cdn.digitaloceanspaces.com/media/launch_images/electron25202_image_20220420085230.jpg",
-        launch_service_provider: {name: 'SpaceX'},
+        launch_service_provider: {name: 'SpaceX', description: "Space Exploration Technologies Corp., known as SpaceX, is an American aerospace manufacturer and space transport services company headquartered in Hawthorne, California. It was founded in 2002 by entrepreneur Elon Musk with the goal of reducing space transportation costs and enabling the colonization of Mars. SpaceX operates from many pads, on the East Coast of the US they operate from SLC-40 at Cape Canaveral Space Force Station and historic LC-39A at Kennedy Space Center. They also operate fro..."},
         vidURLs: [{url: "https://youtube.com/watch?v=s6yBwQSrtFY"}],
         probability: 70,
         rocket: {configuration: {full_name: 'Falcon 9 Block 5', description: "Falcon 9 is a two-stage rocket designed and manufactured by SpaceX for the reliable and safe transport of satellites and the Dragon spacecraft into orbit. The Block 5 variant is the fifth major interval aimed at improving upon the ability for rapid reusability."}},
@@ -26,7 +26,7 @@ const singleLaunch = () => {
         mission_patches: [{image_url: "https://spacelaunchnow-prod-east.nyc3.cdn.digitaloceanspaces.com/media/mission_patch_images/space2520x252_mission_patch_20210606182401.png"}],
         mission: {description: "A batch of 53 satellites for Starlink mega-constellation - SpaceX's project for space-based Internet communication system.", type: "Communications", orbit: {name: 'Low Earth Orbit'}},
         image: "https://spacelaunchnow-prod-east.nyc3.cdn.digitaloceanspaces.com/media/launch_images/electron25202_image_20220420085230.jpg",
-        launch_service_provider: {name: 'SpaceX'},
+        launch_service_provider: {name: 'SpaceX', description: "Space Exploration Technologies Corp., known as SpaceX, is an American aerospace manufacturer and space transport services company headquartered in Hawthorne, California. It was founded in 2002 by entrepreneur Elon Musk with the goal of reducing space transportation costs and enabling the colonization of Mars. SpaceX operates from many pads, on the East Coast of the US they operate from SLC-40 at Cape Canaveral Space Force Station and historic LC-39A at Kennedy Space Center. They also operate fro..."},
         vidURLs: [{url: "https://youtube.com/watch?v=s6yBwQSrtFY"}],
         probability: 70,
         rocket: {configuration: {full_name: 'Falcon 9 Block 5', description: "Falcon 9 is a two-stage rocket designed and manufactured by SpaceX for the reliable and safe transport of satellites and the Dragon spacecraft into orbit. The Block 5 variant is the fifth major interval aimed at improving upon the ability for rapid reusability."}},
@@ -40,7 +40,7 @@ const singleLaunch = () => {
         mission_patches: [{image_url: "https://spacelaunchnow-prod-east.nyc3.cdn.digitaloceanspaces.com/media/mission_patch_images/space2520x252_mission_patch_20210606182401.png"}],
         mission: {description: "A batch of 53 satellites for Starlink mega-constellation - SpaceX's project for space-based Internet communication system.", type: "Communications", orbit: {name: 'Low Earth Orbit'}},
         image: "https://spacelaunchnow-prod-east.nyc3.cdn.digitaloceanspaces.com/media/launch_images/electron25202_image_20220420085230.jpg",
-        launch_service_provider: {name: 'SpaceX'},
+        launch_service_provider: {name: 'SpaceX', description: "Space Exploration Technologies Corp., known as SpaceX, is an American aerospace manufacturer and space transport services company headquartered in Hawthorne, California. It was founded in 2002 by entrepreneur Elon Musk with the goal of reducing space transportation costs and enabling the colonization of Mars. SpaceX operates from many pads, on the East Coast of the US they operate from SLC-40 at Cape Canaveral Space Force Station and historic LC-39A at Kennedy Space Center. They also operate fro..."},
         vidURLs: [{url: "https://youtube.com/watch?v=s6yBwQSrtFY"}],
         probability: 70,
         rocket: {configuration: {full_name: 'Falcon 9 Block 5', description: "Falcon 9 is a two-stage rocket designed and manufactured by SpaceX for the reliable and safe transport of satellites and the Dragon spacecraft into orbit. The Block 5 variant is the fifth major interval aimed at improving upon the ability for rapid reusability."}},
@@ -79,7 +79,7 @@ describe('Single Launch flow', () => {
     cy.intercept('GET', 'https://spacelaunchnow.me/api/ll/2.2.0/launch/upcoming/?limit=20&mode=detailed', singleLaunch()).as('getSingleLaunch')
     cy.visit('http://localhost:3000/launches/2c5447d7-36c5-40fd-88de-47ed6b258bdb')
     .get('.single-launch-page').find('.bookmark-btn').click()
-    .contains('Launch Bookmarked 👍')
+    .contains('Launch Bookmarked')
   });
 
 })
