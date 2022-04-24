595fc7fec0d03368b9a7f34cebe211ba6846c677
Madison Law <madisonlaw1@gmail.com>
2022-04-24 13:55:28 -0600

    Remove comments

--- src/components/Bookmarks/Bookmarks.js
+++ src/components/Bookmarks/Bookmarks.js
@@ -3,28 +3,12 @@ import { Link } from 'react-router-dom';
 import { BookmarkContext } from '../../contexts/BookmarkContext';
 import SavedLaunch from '../SavedLaunch/SavedLaunch';
 import './Bookmarks.css'
+import {IoChevronBackOutline} from "react-icons/io5";
+
 
 
 const Bookmarks = () => {
   const bookmarkData = useContext(BookmarkContext);
-
-
-// TESTING NOTIFICATION API -- WILL NEED TO DEPLOY TO TRULY TEST SINCE ONLY WORKS OVER HTTPS PROTOCOL
-  // const showNotification = () => {
-  //   return new Notification('Go for Launch!', {
-  //     body: "You have an upcoming launch at 11:30am 4/22"
-  //   })
-  // }
-
-  // useEffect(() => {
-  //   if (Notification.permission === 'granted') {
-  //     showNotification()
-  //   } else if (Notification.permission !== 'denied') {
-  //     Notification.requestPermission().then(permission => {
-  //       return permission === 'granted' ? showNotification() : null
-  //     })
-  //   }
-  // }, [])
 
   const renderBookmarkCards = () => {
     return bookmarkData.bookmarks.map(bookmark => {
@@ -44,11 +28,11 @@ const Bookmarks = () => {
   return (
     <section className='bookmark-section'>
       <div className='bookmark-header'>
-        <h1>🚀 My Bookmarked Launches</h1>
-        <Link to='/'><button className='back-to-main'>X</button></Link>
+        <h1 className='bookmark-header-text'>My Bookmarked Launches</h1>
+        <Link to='/'><button className='back-to-main'><IoChevronBackOutline className='back-icon'/></button></Link>
       </div>
       <div className='bookmark-container'>
-        {bookmarkData.bookmarks.length ? renderBookmarkCards() : "No launches bookmarked yet!"}
+        {bookmarkData.bookmarks.length ? renderBookmarkCards() : <h2 className='no-launches'>No launches bookmarked yet!</h2>}
       </div>
     </section>
   );
