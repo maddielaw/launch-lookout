5658022c7f128f6915e68efc1c357e6aa6e5a784
Madison Law <madisonlaw1@gmail.com>
2022-04-24 13:55:53 -0600

    Style error page

--- src/components/Error/Error.js
+++ src/components/Error/Error.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Error.css'
+import {IoIosRocket} from 'react-icons/io'
+import blackhole from '../../assets/blackhole.png'
+
 
 
 const Error = () => {
   return (  
     <section className='error-page'>
       <nav className='error-nav'>
-        <h1 className='header'>🔭 Launch Lookout</h1>
+      <h1 className='error-header'><IoIosRocket className='rocket-icon'/>Launch Lookout</h1>
       </nav>
       <div className='error-text'>
         <h1>Failure to liftoff!</h1>
+        <img className='blackhole-spinning-error' src={blackhole} alt={'blackhole spinning loading'}></img>
         <Link to='/'><button className='error-back-to-main'>Back to Main</button></Link>
       </div>
     </section>
