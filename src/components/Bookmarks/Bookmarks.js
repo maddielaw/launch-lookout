5b0497a7ec38a01ecfbf25fc8930b6b09ae4af83
Madison Law <madisonlaw1@gmail.com>
2022-04-24 14:21:15 -0600

    Format Bookmarks

--- src/components/Bookmarks/Bookmarks.js
+++ src/components/Bookmarks/Bookmarks.js
@@ -1,41 +1,49 @@
-import React, { useContext, useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import { BookmarkContext } from '../../contexts/BookmarkContext';
-import SavedLaunch from '../SavedLaunch/SavedLaunch';
-import './Bookmarks.css'
-import {IoChevronBackOutline} from "react-icons/io5";
-
-
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import { BookmarkContext } from "../../contexts/BookmarkContext";
+import SavedLaunch from "../SavedLaunch/SavedLaunch";
+import "./Bookmarks.css";
+import { IoChevronBackOutline } from "react-icons/io5";
 
 const Bookmarks = () => {
   const bookmarkData = useContext(BookmarkContext);
 
   const renderBookmarkCards = () => {
-    return bookmarkData.bookmarks.map(bookmark => {
-      return <SavedLaunch 
-        id={bookmark.id} 
-        key={bookmark.id} 
-        name={bookmark.name} 
-        company={bookmark.company} 
-        image={bookmark.image} 
-        mission={bookmark.mission}
-        launchDate={bookmark.launchDate} 
-        removeBookmark={bookmarkData.removeBookmark}
-      />
-    })
-  }
+    return bookmarkData.bookmarks.map((bookmark) => {
+      return (
+        <SavedLaunch
+          id={bookmark.id}
+          key={bookmark.id}
+          name={bookmark.name}
+          company={bookmark.company}
+          image={bookmark.image}
+          mission={bookmark.mission}
+          launchDate={bookmark.launchDate}
+          removeBookmark={bookmarkData.removeBookmark}
+        />
+      );
+    });
+  };
 
   return (
-    <section className='bookmark-section'>
-      <div className='bookmark-header'>
-        <h1 className='bookmark-header-text'>My Bookmarked Launches</h1>
-        <Link to='/'><button className='back-to-main'><IoChevronBackOutline className='back-icon'/></button></Link>
+    <section className="bookmark-section">
+      <div className="bookmark-header">
+        <h1 className="bookmark-header-text">My Bookmarked Launches</h1>
+        <Link to="/">
+          <button className="back-to-main">
+            <IoChevronBackOutline className="back-icon" />
+          </button>
+        </Link>
       </div>
-      <div className='bookmark-container'>
-        {bookmarkData.bookmarks.length ? renderBookmarkCards() : <h2 className='no-launches'>No launches bookmarked yet!</h2>}
+      <div className="bookmark-container">
+        {bookmarkData.bookmarks.length ? (
+          renderBookmarkCards()
+        ) : (
+          <h2 className="no-launches">No launches bookmarked yet!</h2>
+        )}
       </div>
     </section>
   );
-}
- 
+};
+
 export default Bookmarks;
