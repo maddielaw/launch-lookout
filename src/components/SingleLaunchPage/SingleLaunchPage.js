b6229cd7fab88d381b89f7287e5faeee6da14289
Madison Law <madisonlaw1@gmail.com>
2022-04-23 18:29:52 -0600

    Add icons

--- src/components/SingleLaunchPage/SingleLaunchPage.js
+++ src/components/SingleLaunchPage/SingleLaunchPage.js
@@ -6,6 +6,8 @@ import { DataContext } from '../../contexts/DataContext';
 import LaunchDetails from '../LaunchDetails/LaunchDetails';
 import './SingleLaunchPage.css'
 import Countdown from '../Countdown/Countdown';
+import {IoChevronBackOutline, IoCheckmarkOutline} from "react-icons/io5";
+
 
 const SingleLaunchPage = ({ id }) => {
   const allLaunchData = useContext(DataContext)
@@ -40,24 +42,25 @@ const SingleLaunchPage = ({ id }) => {
       <div className='titles-div'>
         <div className='title-text'>
           <h1 className='mission-name'>{launch.name}</h1>
-          <p>{format(new Date(launch.window_start), "E. MMMM dd, yyy, p")}</p>
-          {launch.status.name !== 'Launch Successful' && <p>Status: {launch.status.name}</p>}
+          <p className='single-launch-date'>{format(new Date(launch.window_start), "E. MMMM dd, yyy, p")}</p>
+          {launch.status.name !== 'Launch Successful' && <p className='launch-status'>Status: {launch.status.name}</p>}
         </div>
         <div className='single-countdown-container'>
           <Countdown date={launch.window_start} status={launch.status.name} statusDescription={launch.status.description}/>
         </div>
         <div className='title-buttons'>
-          <Link to='/'>
-            <button className='back-to-main'>Back to Main</button>
-          </Link>
-          <Link to='/bookmarks'>
-            <button>My Bookmarks</button>
-          </Link>
-          {isBookmarked ? <button className='bookmark-btn' disabled>Launch Bookmarked</button> : <button className='bookmark-btn' onClick={() => addBookmark()}>Bookmark this Launch</button>}
+          <div className='routing-btns'>
+            <Link to='/'>
+              <button className='back-to-main'><IoChevronBackOutline className='back-icon'/></button>
+            </Link>
+            <Link to='/bookmarks'>
+              <button className='to-bookmarks'>My Bookmarks</button>
+            </Link>
+          </div>
+          {isBookmarked ? <button className='bookmark-btn bookmarked' disabled><IoCheckmarkOutline className='check-icon'/>Launch Bookmarked</button> : <button className='bookmark-btn' onClick={() => addBookmark()}>Bookmark this Launch</button>}
         </div>
       </div>
       <LaunchDetails launch={launch}/>
-
     </section>
   );
 }
