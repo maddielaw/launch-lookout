d3174527e557dc36ef973c845bc354a35481f822
Madison Law <madisonlaw1@gmail.com>
2022-04-24 13:57:38 -0600

    Style saved launch cards

--- src/components/SavedLaunch/SavedLaunch.js
+++ src/components/SavedLaunch/SavedLaunch.js
@@ -5,21 +5,21 @@ import './SavedLaunch.css'
 const SavedLaunch = ({id, name, company, image, mission, launchDate, removeBookmark}) => {
   return (
     <section className='saved-launch-card'>
-      <div className='date-delete'>
-        <p>{launchDate}</p>
+      <header className='date-delete'>
+        <p className='saved-launch-date'>{launchDate}</p>
         <button className='remove-bookmark-btn' onClick={() => removeBookmark(id)}>X</button>
-      </div>
-      <div className='saved-launch-details-container'>
-        <div className='saved-img-container' >
-          <img className='saved-launch-img'src={image} />
-        </div>
+      </header>
+      <section className='saved-img-container'>
+        <img className='saved-launch-img'src={image} />
+      </section>
+      <footer className='saved-launch-details-container'>
         <div className='saved-launch-details'>
-          <h2>{name}</h2>
-          <p>{company}</p>
-          <p>{mission}</p>
+          <h2 className='saved-name'>{name}</h2>
+          <p className='saved-company'>{company}</p>
+          {mission.length > 159 ? <p className='saved-mission'>{`${mission.substring(0, 159)} ...`}</p> : <p className='saved-mission'>{mission}</p> }
         </div>
-          <Link to={`/launches/${id}`}><button>View Launch Details</button></Link>
-      </div>
+          <Link to={`/launches/${id}`}><button className='launch-details-btn'>View Launch Details</button></Link>
+      </footer>
     </section>
 
 
