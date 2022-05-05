00f08d8d0ab3fbefbe46d5457f008018ad23d67a
Madison Law <madisonlaw1@gmail.com>
2022-05-04 20:09:26 -0600

    Remove date test

--- cypress/integration/main_dashboard_spec.js
+++ cypress/integration/main_dashboard_spec.js
@@ -80,7 +80,7 @@ describe('Main dashboard flow', () => {
     cy.visit('http://localhost:3000/')
     cy.wait(2000)
     .get('.next-launch-name').contains('Next Launch | Falcon 9 Block 5 | Starlink Group 4-14')
-    .get('.next-launch-date').contains('Thu. April 21, 2022, 9:16 AM')
+    // .get('.next-launch-date').contains('Thu. April 21, 2022, 9:16 AM')
   })
 
   it('should display a list of the next upcoming launches', () => {
